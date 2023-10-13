a43ff3861a482e3d2e80c52e7ce99869c81538e4
Su,Yen,Wen <kevin@sukon.idv.tw>
2023-10-13 15:43:32 +0800

    Use OpenCV functions to complete the dilation and erosion

--- main.py
+++ main.py
@@ -39,10 +39,19 @@ def adaptive_threshold(img, max = 255, block_size = 21, C = 0):
 
 
 if __name__ == '__main__':
+
+
     img = cv2.imread("sample/test1.bmp", cv2.IMREAD_GRAYSCALE)
     img_bin = adaptive_threshold(img, 255, 19, 15)
-    
-    cv2.imshow("fig1", img_bin)
+
+    #NOTE 這裡如果要使用需要自己寫，但重要性未知，先跳過
+    kernel = np.ones((2,2), np.uint8)
+    dilation = cv2.dilate(img_bin, kernel, iterations = 1) #侵蝕
+    erosion = cv2.erode(dilation, kernel, iterations = 1)#膨脹
+    ###############
+
+    # cv2.imshow("fig1", img_bin)
+    cv2.imshow('erosion', erosion)
     cv2.waitKey() 
     cv2.destroyAllWindows()
 
