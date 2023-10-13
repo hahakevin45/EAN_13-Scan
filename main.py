dfcc77caaf8f0bfac152bb8999a7c0a0982d1c76
Su,Yen,Wen <kevin@sukon.idv.tw>
2023-10-13 15:43:32 +0800

    Use OpenCV functions to complete the dilation and erosion

--- main.py
+++ main.py
@@ -2,48 +2,52 @@ import cv2
 import numpy as np
 
 
-
-
-#計算傳入的矩陣平均數值
-def img_average (img):
+# 計算傳入的矩陣平均數值
+def img_average(img) -> float:
     img_size = np.shape(img)
-    img_average_value = np.sum(img,axis = (0,1))/(img_size[0]*img_size[1])
+    img_average_value = np.sum(img, axis=(0, 1))/(img_size[0]*img_size[1])
     return img_average_value
 
 
-#二值化矩陣
-def binarization(img, max = 255, C=0):
+# 二值化矩陣
+def binarization(img, max=255, C=0):
     img_size = np.shape(img)
-    img_average_value = img_average(img)    
+    img_average_value = img_average(img)
     for i in range(img_size[0]):
         for j in range(img_size[1]):
             if img[i][j] > (img_average_value-C):
                 img[i][j] = max
             elif img[i][j] <= (img_average_value-C):
                 img[i][j] = 0
-    return img;
+    return img
 
 
-#使用Adaptive Threshold 二值化矩陣
-def adaptive_threshold(img, max = 255, block_size = 21, C = 0):
+# 使用Adaptive Threshold 二值化矩陣
+def adaptive_threshold(img, max=255, block_size=21, C=0):
     img_size = np.shape(img)
 
-    for i in  range(img_size[0]//block_size):
-        for j in  range(img_size[1]//block_size):
+    for i in range(img_size[0]//block_size):
+        for j in range(img_size[1]//block_size):
             temp_block = np.ones((block_size, block_size))
-            temp_block = img[(i)*block_size:(i+1)*block_size, (j)*block_size:(j+1)*block_size]
-            temp_block = binarization(temp_block, max, C)   
-    return img;
-
-
+            temp_block = img[(i)*block_size:(i+1)*block_size,
+                             (j)*block_size:(j+1)*block_size]
+            temp_block = binarization(temp_block, max, C)
+    return img
 
 
 if __name__ == '__main__':
+
     img = cv2.imread("sample/test1.bmp", cv2.IMREAD_GRAYSCALE)
-    img_bin = adaptive_threshold(img, 255, 19, 15)
-    
-    cv2.imshow("fig1", img_bin)
-    cv2.waitKey() 
+    img_bin = adaptive_threshold(img, 255, 19, 10)
+
+    # NOTE 功能:侵蝕然後膨脹，但重要性未知，先使用內建函數跳過
+    kernel = np.ones((2, 2), np.uint8)
+    dilation = cv2.dilate(img_bin, kernel, iterations=1)  # 侵蝕
+    erosion = cv2.erode(dilation, kernel, iterations=1)  # 膨脹
+    ###############
+
+    # cv2.imshow("fig1", img_bin)
+    cv2.imshow('erosion', erosion)
+    cv2.imshow('bin', img_bin)
+    cv2.waitKey()
     cv2.destroyAllWindows()
-
-
